b6876170de733ef7bef8a427feb77bb81e80b24e
BrandonCooper614 <118305338+BrandonCooper614@users.noreply.github.com>
2022-11-25 09:03:12 -0500

    Update quizgame.py

--- quizgame.py
+++ quizgame.py
@@ -7,3 +7,17 @@ if playing != "yes":
 
 print("Okay! Lets play :)")
 
+import random
+
+number1 = random.randint(0,12)
+number2 = random.randint(0,12)
+
+result = number1 * number2
+guess = input(f"what is {number1} x {number2} ?  ")
+guess = int(guess)
+
+if guess == result:
+    print ("Great job! You got the right answer!")
+else:
+    print ("Not quite. Try again.  Practice makes perfect!")
+
